48006b3f95776deed60808eb27689e7cba33cda0
Nick Randall <nick.randall@domo.com>
2015-10-01 13:25:35 -0600

    adding new arrow function syntax to life-cycle events on layer

--- src/chart.js
+++ src/chart.js
@@ -13,15 +13,15 @@ class BarChart extends Koto {
     this.configs = configs;
 
     // Scales
-    chart.x = d3.scale.linear()
+    this.x = d3.scale.linear()
       .range([0, this.config('width')]);
 
-    chart.y = d3.scale.linear()
+    this.y = d3.scale.linear()
       .domain([0, 100])
       .rangeRound([0, this.config('height')]);
 
     // add a layer
-    chart.layer('bars', this.base.append('g'), {
+    this.layer('bars', this.base.append('g'), {
       // destructuring ftw
       dataBind(data) {
         return this.selectAll('rect')
@@ -31,31 +31,27 @@ class BarChart extends Koto {
         return this.append('rect');
       }
     })
-    // lifecycle events
-    .on('enter', function() {
-      var length = this.data().length;
-      this.attr('x', (d, i) => chart.x(i + 1) - 0.5 )
-        .attr('y', (d) => chart.config('height') - chart.y(d.value) - 0.5)
-        .attr('width', chart.config('width') / length)
+    // lifecycle events (Arrow function syntax)
+    .on('enter', selection => {
+      var length = this._length = selection.data().length;
+      selection.attr('x', (d, i) => this.x(i + 1) - 0.5 )
+        .attr('y', (d) => this.config('height') - this.y(d.value) - 0.5)
+        .attr('width', this.config('width') / length)
         .style('fill', 'steelBlue')
-        .attr('height', d => chart.y(d.value));
+        .attr('height', d => this.y(d.value));
     })
-    .on('enter:transition', function() {
-      this.duration(1000)
-        .attr('x', (d, i) => chart.x(i) - 0.5);
+    .on('merge:transition', selection => {
+      selection.duration(1000)
+        .attr('x', (d, i) => this.x(i) - 0.5);
     })
-    .on('update:transition', function() {
-      this.duration(1000)
-        .attr('x', (d, i) => chart.x(i) - 0.5);
-    })
-    .on('exit:transition', function() {
-      this.duration(1000)
-        .attr('x', (d, i) => chart.x(i - 1) - 0.5)
+    .on('exit:transition', selection => {
+      selection.duration(1000)
+        .attr('x', (d, i) => this.x(i - 1) - 0.5)
         .remove();
     });
 
-    // add another layer
-    chart.layer('labels', this.base.append('g'), {
+    // add another layer 
+    this.layer('labels', this.base.append('g'), {
       dataBind(data) {
         return this.selectAll('text')
           .data(data, d => d.time);
@@ -64,19 +60,19 @@ class BarChart extends Koto {
         return this.append('text');
       }
     })
+    // non arrow function syntax
     .on('enter', function() {
-      this.attr('x', (d, i) => chart.x(i + 1) - 0.5)
+      var length = this.data().length;
+      this
+        .attr('x', (d, i) => chart.x(i + 1) + ((chart.config('width') / length) / 2))
         .attr('y', d => chart.config('height') - chart.y(d.value) - 15)
         .style('fill', 'steelBlue')
+        .style('text-anchor', 'middle')
         .text(d => d.value);
     })
-    .on('enter:transition', function() {
+    .on('merge:transition', function() {
       this.duration(1000)
-        .attr('x', (d, i) => chart.x(i) - 0.5);
-    })
-    .on('update:transition', function() {
-      this.duration(1000)
-        .attr('x', (d, i) => chart.x(i) - 0.5);
+        .attr('x', (d, i) => chart.x(i) + ((chart.config('width') / chart._length) / 2));
     })
     .on('exit:transition', function() {
       this.duration(1000)
